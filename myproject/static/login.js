e71e3d78bceb972fc99daf18f2e27e32cc568fef
Kwaku Ofosu <kbofosutuffour@wm.edu>
2023-10-23 01:03:07 -0400

    initial commit

--- myproject/static/login.js
+++
@@ -1,9 +0,0 @@
-
-function togglePassword() {
-    //switches between visible and hidden password
-
-    let input = document.getElementById("password");
-    let oldState = input.getAttribute("type");
-    let newState = (oldState == "password") ? "text" : "password";
-    input.setAttribute("type", newState);
-}
